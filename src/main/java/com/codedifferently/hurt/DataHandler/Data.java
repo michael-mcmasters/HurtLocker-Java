6f54ae3f91cc89bddcd9dae190037f1c596ccf2d
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-31 14:37:30 -0500

    Give properties to Data class

--- src/main/java/com/codedifferently/hurt/DataHandler/Data.java
+++ src/main/java/com/codedifferently/hurt/DataHandler/Data.java
@@ -1,4 +1,10 @@
 package com.codedifferently.hurt.DataHandler;
 
 public class Data {
+
+    protected String name;
+    protected String price;
+    protected String type;
+    protected String expiration;
+
 }
