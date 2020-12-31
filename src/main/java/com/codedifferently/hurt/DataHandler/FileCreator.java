dea251388bb37a788808640c7bef0b65d91604c0
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-31 16:16:49 -0500

    Print data dummy to output_2.txt

--- src/main/java/com/codedifferently/hurt/DataHandler/FileCreator.java
+++ src/main/java/com/codedifferently/hurt/DataHandler/FileCreator.java
@@ -3,15 +3,33 @@ package com.codedifferently.hurt.DataHandler;
 import com.codedifferently.hurt.DataHandler.Interfaces.IDataParser;
 import com.codedifferently.hurt.DataHandler.Interfaces.IFileCreator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileCreator implements IFileCreator {
 
-    public FileCreator(IDataParser dataParser) {
+    private IDataParser dataParser;
 
+    public FileCreator(IDataParser dataParser) {
+        this.dataParser = dataParser;
     }
 
     @Override
     public void createLogFile() {
+        File file = new File("output_2.txt");
 
+        try {
+            //String output = getData();
+            String output = "wow";
+
+            FileWriter writer = new FileWriter(file);
+            writer.write(output);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("FAILED");
+        }
     }
 
 }
