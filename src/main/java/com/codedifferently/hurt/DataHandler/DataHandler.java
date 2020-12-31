07f267c37690e4df15c98d97d85c2a0248e3cadf
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-31 14:34:36 -0500

    Add properties and functions to DataHandler

--- src/main/java/com/codedifferently/hurt/DataHandler/DataHandler.java
+++ src/main/java/com/codedifferently/hurt/DataHandler/DataHandler.java
@@ -1,4 +1,27 @@
 package com.codedifferently.hurt.DataHandler;
 
+import com.codedifferently.hurt.DataHandler.Interfaces.IDataParser;
+import com.codedifferently.hurt.DataHandler.Interfaces.IFileCreator;
+import com.codedifferently.hurt.DataHandler.Interfaces.IRawDataParser;
+
+import java.util.List;
+
 public class DataHandler {
+
+    private List<Data> data;
+    private IRawDataParser rawDataParser;
+    private IDataParser dataParser;
+    private IFileCreator fileCreator;
+
+    public DataHandler(String rawData) {
+        data = formatData(rawData);
+    }
+
+    private List<Data> formatData(String rawData) {
+        return null;
+    }
+
+    public void logDataToFile() {
+
+    }
 }
